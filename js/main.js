4e49d5cf13ac0fed6b8fddc9b2088a50862effab
akDeveloper <andreas.kollaros@gmail.com>
2014-12-20 15:52:42 +0200

    add simple error handler

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
+window.onerror = function(messsage, url, line, col, error) {
+    // Available in gecko engines.
+    var extra = !col ? '' : '\ncolumn: ' + col;
+    extra += !error ? '' : '\nerror: ' + error;
+
+    console.log("Error: " + message + "\nurl: " + url + "\nline: " + line + extra);
+
+    return true;
+};
+
 require.config({
     paths: {
         templates: '../templates',
         jquery: 'libs/jquery/jquery-min',
         underscore: 'libs/underscore/underscore-min',
         backbone: 'libs/backbone/backbone-min',
-        bootstrap: 'libs/bootstrap-min',
+        bootstrap: 'libs/bootstrap.min',
         mustache: 'libs/mustache/mustache'
     }
 });
